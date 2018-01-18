b9084d355d94ddca1b1c7ac74e7cea063eacc305
Christian Nikolov <christian@newton.codes>
2018-01-18 05:41:26 +0200

    Fix build script

--- dev/build-all.sh
+++ dev/build-all.sh
@@ -4,10 +4,10 @@ dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 set -e
 
-cd ${dir}/../5.6 && docker build -t newtoncodes/mysql .
-cd ${dir}/../5.6 && docker build -t newtoncodes/mysql:5.6 .
-cd ${dir}/../5.6-fpm && docker build -t newtoncodes/mysql:5.6-fpm .
-cd ${dir}/../7.1 && docker build -t newtoncodes/mysql:7.1 .
-cd ${dir}/../7.1-fpm && docker build -t newtoncodes/mysql:7.1-fpm .
-#cd ${dir}/../7.2 && docker build -t newtoncodes/mysql:7.2 .
-#cd ${dir}/../7.2-fpm && docker build -t newtoncodes/mysql:7.2-fpm .
+cd ${dir}/../5.6 && docker build -t newtoncodes/php .
+cd ${dir}/../5.6 && docker build -t newtoncodes/php:5.6 .
+cd ${dir}/../5.6-fpm && docker build -t newtoncodes/php:5.6-fpm .
+cd ${dir}/../7.1 && docker build -t newtoncodes/php:7.1 .
+cd ${dir}/../7.1-fpm && docker build -t newtoncodes/php:7.1-fpm .
+#cd ${dir}/../7.2 && docker build -t newtoncodes/php:7.2 .
+#cd ${dir}/../7.2-fpm && docker build -t newtoncodes/php:7.2-fpm .
