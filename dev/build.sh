4798c69d81828d938e96525ad9eb06c99e305a8d
Christian Nikolov <christian@newton.codes>
2018-01-18 04:27:10 +0200

    Fixed fpm tags in build scripts.

--- dev/build.sh
+++ dev/build.sh
@@ -6,6 +6,8 @@ v="$1"
 if [ "$v" = "" ] || [ "$v" = "5.6" ]; then
     cd ${dir}/../5.6 && docker build -t newtoncodes/php .
     cd ${dir}/../5.6 && docker build -t newtoncodes/php:5.6 .
+    cd ${dir}/../5.6-fpm && docker build -t newtoncodes/php:5.6-fpm .
 else
     cd ${dir}/../${v} && docker build -t newtoncodes/php:${v} .
+    cd ${dir}/../${v}-fpm && docker build -t newtoncodes/php:${v}-fpm .
 fi
--- dev/push.sh
+++ dev/push.sh
@@ -5,6 +5,8 @@ v="$1"
 if [ "$v" = "" ] || [ "$v" = "5.6" ]; then
     docker push newtoncodes/php
     docker push newtoncodes/php:5.6
+    docker push newtoncodes/php:5.6-fpm
 else
     docker push newtoncodes/php:${v}
+    docker push newtoncodes/php:${v}-fpm
 fi
